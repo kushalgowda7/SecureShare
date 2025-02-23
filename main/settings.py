35cd77027264d9d83b4dc1768188d19de52afab2
kushalgowda.7 <kushalgowda1112@gmail.com>
2025-02-23 15:17:18 +0530

    Removed Secure Key

--- main/settings.py
+++ main/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY","2pw+102q_fdpvcadyhz8x173gw7c_qwe6e2$nsi_v257)c(h*")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
